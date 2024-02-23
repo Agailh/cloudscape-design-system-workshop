0d6b21132ad7fa163ee68c2c0ab6d142ff608af9
Agailh <agailham86@gmail.com>
2024-02-23 13:22:33 +0700

    commit

--- src/pages/home/app.tsx
+++ src/pages/home/app.tsx
@@ -1,10 +1,37 @@
-// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: MIT-0
 import React from 'react';
+import TopNavigation from '@cloudscape-design/components/top-navigation';
+import AppLayout from '@cloudscape-design/components/app-layout';
+
+import './styles.css';
+
 export default function App() {
   return (
     <>
-      Hello <a href="https://cloudscape.design/">Cloudscape Design System</a>!
+      <div id="top-nav">
+        <TopNavigation
+          identity={{
+            logo: { src: '/logo.svg', alt: 'Chocolate Factory Logo' },
+            title: 'Chocolate Factory',
+            href: '/home/index.html',
+          }}
+          i18nStrings={{
+            overflowMenuTriggerText: 'More',
+            overflowMenuTitleText: 'All',
+          }}
+        />
+      </div>
+      <AppLayout
+        headerSelector="#top-nav"
+        ariaLabels={{
+          navigation: 'Navigation drawer',
+          navigationClose: 'Close navigation drawer',
+          navigationToggle: 'Open navigation drawer',
+          notifications: 'Notifications',
+          tools: 'Help panel',
+          toolsClose: 'Close help panel',
+          toolsToggle: 'Open help panel',
+        }}
+      />
     </>
   );
 }
